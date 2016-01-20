e29dffca898c296fd3baabde20cb1c226b40e329
Rita Zhang <rita.z.zhang@gmail.com>
2016-01-19 16:44:59 -0800

    Document test sample app for S3Proxy

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AWS SDK for Java Sample Project
 
-A simple Java application illustrating usage of the AWS SDK for Java.
+A simple Java application illustrating usage of the AWS S3 SDK for Java.
 
 ## Requirements
 
@@ -9,35 +9,41 @@ be installed by building the maven package:
     
     mvn package
 
-## Basic Configuration
-
-You need to set up your AWS security credentials before the sample code is able
-to connect to AWS. You can do this by creating a file named "credentials" at ~/.aws/ 
-(C:\Users\USER_NAME\.aws\ for Windows users) and saving the following lines in the file:
-
-    [default]
-    aws_access_key_id = <your access key id>
-    aws_secret_access_key = <your secret key>
-
-See the [Security Credentials](http://aws.amazon.com/security-credentials) page
-for more information on getting your keys.
-
 ## Running the S3 sample
 
 ### Prerequisites
-You will need to go to [IAM policies page](https://console.aws.amazon.com/iam/home?#policies), search for the String "S3,"
-and "Attach" the "AmazonS3FullAccess" policy to the user whose credentials exist in 
-your `~/.aws/credentials` file. Otherwise, you will likely get a `AmazonServiceException`/`Access Denied`/`403` error.
+#### Azure
+If you are using Microsoft Azure, you will need to go to the Microsoft Azure portal, Follow this [guide](https://azure.microsoft.com/en-us/documentation/articles/storage-create-storage-account/#create-a-storage-account) to create a storage account and to get the key and secret for your storage account. You can also use the [azure cli](https://azure.microsoft.com/en-us/documentation/articles/storage-azure-cli/) to create and retrieve storage account information.
 
-This sample application connects to Amazon's [Simple Storage Service (S3)](http://aws.amazon.com/s3),
-creates a bucket, and uploads a file to that bucket. The code will generate a
-bucket name for you, as well as an example file to upload. All you need to do
-is run it:
-
-    mvn clean compile exec:java
-
-When you start making your own buckets, the S3 documentation provides a good overview
+#### AWS
+If you are using AWS S3, follow this [guide](http://docs.aws.amazon.com/AmazonS3/latest/gsg/SigningUpforS3.html) to signup for S3 account and to retrieve access key and secret. The S3 documentation provides a good overview
 of the [restrictions for bucket names](http://docs.aws.amazon.com/AmazonS3/latest/dev/BucketRestrictions.html).
+
+This sample application connects to an S3 API compatible storage backend. 
+- It can create a container (on Microsoft Azure) or a bucket (on AWS S3).
+- It can list all containers or all buckets in your storage account.
+- It can create an example file to upload to a container/bucket.
+- It can upload a large file using multipart upload UploadPartRequest.
+- It can upload a large file using multipart upload TransferManager.
+- It can list all objects in a container/bucket.
+- It can delete an object in a container/bucket.
+- It can delete a container/bucket.
+
+
+All you need to do is run it with:
+
+```
+mvn clean compile exec:java -Dkey=<STORAGE ACCOUNT KEY> -Dsecret=<STORAGE ACCOUNT SECRET> -Dbucketname=<BUCKETNAME OR CONTAINER NAME TO CREATE> -Ds3endpoint=<S3PROXY URL> -Dfilepath=<FULLPATH LOCATION OF DEMO.ZIP FILE>/demo.zip
+```
+
+For `<S3PROXY URL>`, here are some options depending on where your S3Proxy instance is hosted. For more information of how to host S3Proxy, checkout the [s3proxydocker project](https://github.com/ritazh/s3proxydocker).
+
+- S3Proxy running as a container app on Cloud Foundry: `-Ds3endpoint=<PUBLIC-IP-OF-CLOUD-FOUNDRY-INSTANCE>`
+- S3Proxy running as a container app on Dokku: `-Ds3endpoint=s3proxy.<PUBLIC-IP-OF-DOKKU-INSTANCE>`
+- S3Proxy running as a container app locally using Docker machine: `-Ds3endpoint=<docker-machine IP:8080>`
+- S3Proxy running as a jetty app locally on port 8000: `-Ds3endpoint=:http://localhost:8000`
+
+If you are hosting S3Proxy on a local machine, ensure you have completed the [Updating Hosts File](https://github.com/ritazh/s3proxydocker#updating-hosts-file) step so you can access subdomains over that ip.
 
 ## License
 
--- src/main/java/com/amazonaws/samples/S3Sample.java
+++ src/main/java/com/amazonaws/samples/S3Sample.java
@@ -16,27 +16,48 @@ package com.amazonaws.samples;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.Protocol;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.event.ProgressEvent;
+import com.amazonaws.event.ProgressListener;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.CompleteMultipartUploadRequest;
 import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.InitiateMultipartUploadRequest;
+import com.amazonaws.services.s3.model.InitiateMultipartUploadResult;
 import com.amazonaws.services.s3.model.ListObjectsRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PartETag;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.model.UploadPartRequest;
+import com.amazonaws.services.s3.model.UploadPartResult;
+import com.google.common.io.ByteSource;
+import com.google.common.io.Files;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.Upload;
+import com.amazonaws.util.IOUtils;
 
 /**
  * This sample demonstrates how to make basic requests to Amazon S3 using
@@ -46,33 +67,65 @@ import com.amazonaws.services.s3.model.S3ObjectSummary;
  * account, and be signed up to use Amazon S3. For more information on
  * Amazon S3, see http://aws.amazon.com/s3.
  * <p>
- * <b>Important:</b> Be sure to fill in your AWS access credentials in
- * ~/.aws/credentials (C:\Users\USER_NAME\.aws\credentials for Windows
- * users) before you try to run this sample.
+ * <b>Important:</b> Be sure to provide your access credentials in
+ * [TODO: Add file path for configs] before you try to run this sample.
  */
 public class S3Sample {
 
     public static void main(String[] args) throws IOException {
-        /*
-         * Create your credentials file at ~/.aws/credentials (C:\Users\USER_NAME\.aws\credentials for Windows users) 
-         * and save the following lines after replacing the underlined values with your own.
-         *
-         * [default]
-         * aws_access_key_id = YOUR_ACCESS_KEY_ID
-         * aws_secret_access_key = YOUR_SECRET_ACCESS_KEY
-         */
 
-        AmazonS3 s3 = new AmazonS3Client();
-        Region usWest2 = Region.getRegion(Regions.US_WEST_2);
-        s3.setRegion(usWest2);
+        String s3proxydemoCredentialKey = null;
+        s3proxydemoCredentialKey = System.getProperty("key");
+        String s3proxydemoCredentialSecret = null;
+        s3proxydemoCredentialSecret = System.getProperty("secret");
+        String s3proxydemoBucketname = null;
+        s3proxydemoBucketname = System.getProperty("bucketname");
+        String s3proxydemoEndpint = null;
+        s3proxydemoEndpint = System.getProperty("s3endpoint");
+        String s3proxydemoFilepath = null;
+        s3proxydemoFilepath = System.getProperty("filepath");
 
-        String bucketName = "my-first-s3-bucket-" + UUID.randomUUID();
+
+        if (s3proxydemoCredentialKey == null || s3proxydemoCredentialKey.length() == 0) {
+            System.err.println("-Dkey cannot be empty");
+            System.exit(1);
+        }
+        if (s3proxydemoCredentialSecret == null || s3proxydemoCredentialSecret.length() == 0) {
+            System.err.println("-Dsecret cannot be empty");
+            System.exit(1);
+        }
+        if (s3proxydemoBucketname == null || s3proxydemoBucketname.length() == 0) {
+            System.err.println("-Dbucketname cannot be empty");
+            System.exit(1);
+        }
+        if (s3proxydemoEndpint == null || s3proxydemoEndpint.length() == 0) {
+            System.err.println("-Ds3endpoint cannot be empty");
+            System.exit(1);
+        }
+        if (s3proxydemoFilepath == null || s3proxydemoFilepath.length() == 0) {
+            System.err.println("-Dfilepath cannot be empty");
+            System.exit(1);
+        }
+
+    	// To overcome etag issue
+    	System.setProperty("com.amazonaws.services.s3.disablePutObjectMD5Validation", "1"); 
+    	
+    	AWSCredentials credentials = new BasicAWSCredentials(s3proxydemoCredentialKey, s3proxydemoCredentialSecret);
+    	ClientConfiguration clientConfig = new ClientConfiguration().withSignerOverride("S3SignerType");
+    	clientConfig.setProtocol(Protocol.HTTP);
+    	AmazonS3 s3 = new AmazonS3Client(credentials, clientConfig);
+
+        String bucketName = s3proxydemoBucketname;
         String key = "MyObjectKey";
+        String keylarge = "MyObjectKeyUploadPartRequest.zip";
+        String keylargeTM = "MyObjectKeyTransferManager.zip";
 
         System.out.println("===========================================");
-        System.out.println("Getting Started with Amazon S3");
+        System.out.println("Getting Started with S3 Java Test Client");
         System.out.println("===========================================\n");
-
+    	
+        s3.setEndpoint(s3proxydemoEndpint);
+    	 
         try {
             /*
              * Create a new S3 bucket - Amazon S3 bucket names are globally unique,
@@ -82,9 +135,17 @@ public class S3Sample {
              * You can optionally specify a location for your bucket if you want to
              * keep your data closer to your applications or users.
              */
-            System.out.println("Creating bucket " + bucketName + "\n");
-            s3.createBucket(bucketName);
-
+            try{ 
+                System.out.println("Creating bucket " + bucketName + "\n");
+                s3.createBucket(bucketName);
+            }catch(AmazonServiceException ase) {
+                System.out.println("Error Message:    " + ase.getMessage());
+                System.out.println("HTTP Status Code: " + ase.getStatusCode());
+                System.out.println("AWS Error Code:   " + ase.getErrorCode());
+                if(ase.getErrorCode().compareTo("BucketAlreadyOwnedByYou") == 0){
+                    System.out.println("BucketAlreadyOwnedByYou Skipping this...");
+                }
+            }
             /*
              * List the buckets in your account
              */
@@ -121,7 +182,7 @@ public class S3Sample {
             S3Object object = s3.getObject(new GetObjectRequest(bucketName, key));
             System.out.println("Content-Type: "  + object.getObjectMetadata().getContentType());
             displayTextInputStream(object.getObjectContent());
-
+            
             /*
              * List objects in your bucket by prefix - There are many options for
              * listing the objects in your bucket.  Keep in mind that buckets with
@@ -139,13 +200,154 @@ public class S3Sample {
                         "(size = " + objectSummary.getSize() + ")");
             }
             System.out.println();
+            
+            /*
+             * Multipart-upload an object to your bucket using UploadPartRequest
+             * 
+             */
+            System.out.println("Uploading a large file to storage using multipart upload\n");
+        	String filePath = s3proxydemoFilepath; 
+        	
+        	 //Using UploadPartRequest
+            int size = 10000000;
+            
+            InitiateMultipartUploadRequest initRequest =
+                    new InitiateMultipartUploadRequest(bucketName, keylarge);
+            InitiateMultipartUploadResult initResponse =
+                    s3.initiateMultipartUpload(initRequest);
+            String uploadId = initResponse.getUploadId();
+            InputStream is = new FileInputStream(filePath);
+            File file = new File (filePath);
+           
+            ByteSource byteSource = Files.asByteSource(file);
+            long fileSize = byteSource.size();
+            System.out.println("File size: " + fileSize);
+            
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType("application/unknown");
+            metadata.setContentLength(size);
+            UploadPartRequest uploadRequest1 = new UploadPartRequest()
+                    .withBucketName(bucketName).withKey(keylarge)
+                    .withUploadId(uploadId).withPartNumber(1)
+                    .withInputStream(byteSource.openStream())
+                    .withObjectMetadata(metadata)
+                    .withPartSize(size);
+
+            UploadPartResult uploadPartResult1 = s3.uploadPart(uploadRequest1);
+            PartETag partETag1 = uploadPartResult1.getPartETag();
+            System.out.println(partETag1.getETag());
+
+            metadata.setContentLength(fileSize-size);
+            UploadPartRequest uploadRequest2 = new UploadPartRequest()
+                    .withBucketName(bucketName).withKey(keylarge)
+                    .withUploadId(uploadId).withPartNumber(2)
+                    .withInputStream(byteSource
+                            .slice(size, fileSize - size).openStream())
+                    .withObjectMetadata(metadata)
+                    .withPartSize(fileSize-size);
+
+            UploadPartResult uploadPartResult2 = s3.uploadPart(uploadRequest2);
+            PartETag partETag2 = uploadPartResult2.getPartETag();
+            System.out.println(partETag2.getETag());
+            
+            List<PartETag> partETagList = new ArrayList<PartETag>();
+            partETagList.add(new PartETag(1, partETag1.getETag()));
+            partETagList.add(new PartETag(2, partETag2.getETag()));
+
+            CompleteMultipartUploadRequest completeRequest = new
+                    CompleteMultipartUploadRequest(
+                    bucketName,
+                    keylarge,
+                    uploadId,
+                    partETagList);
+            s3.completeMultipartUpload(completeRequest);
+            
+            /*
+             * List objects in your bucket by prefix - There are many options for
+             * listing the objects in your bucket.  Keep in mind that buckets with
+             * many objects might truncate their results when listing their objects,
+             * so be sure to check if the returned object listing is truncated, and
+             * use the AmazonS3.listNextBatchOfObjects(...) operation to retrieve
+             * additional results.
+             */
+            System.out.println("Listing objects");
+            objectListing = s3.listObjects(new ListObjectsRequest()
+                    .withBucketName(bucketName)
+                    .withPrefix("My"));
+            for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+                System.out.println(" - " + objectSummary.getKey() + "  " +
+                        "(size = " + objectSummary.getSize() + ")");
+            }
+            System.out.println();
+           
+            /*
+             * Multipart-upload an object to your bucket using TransferManager
+             * 
+             */
+        	TransferManager tm = new TransferManager(s3);      
+        	System.out.println("Using TransferManager...");
+          
+        	ObjectMetadata metadataTM = new ObjectMetadata();
+        	metadataTM.setContentType("application/unknown");
+	          File fileTM = new File(filePath);
+	          InputStream isTM = new FileInputStream(filePath);
+	          metadataTM.setContentLength(fileTM.length());
+          
+	        final Upload upload = tm.upload(bucketName, keylargeTM, isTM, metadataTM);
+        	
+            System.out.println("uploading...");
+            if (upload.isDone() == false) {
+                System.out.println("Transfer: " + upload.getDescription());
+                System.out.println("  - State: " + upload.getState());
+            }
+            
+            upload.addProgressListener(new ProgressListener() {
+                // This method is called periodically as your transfer progresses
+                public void progressChanged(ProgressEvent progressEvent) {
+                    System.out.print(".");
+                }
+            });
+            
+            try {
+            	// Or you can block and wait for the upload to finish
+            	upload.waitForCompletion();
+            	System.out.println("Upload complete.");
+            	
+            } catch (AmazonClientException amazonClientException) {
+            	System.out.println("Unable to upload file, upload was aborted.");
+            	amazonClientException.printStackTrace();
+            } catch (InterruptedException interruptException) {
+            	System.out.println("Unable to upload file, upload was interrupted.");
+            	interruptException.printStackTrace();
+            }
+
+
+            /*
+             * List objects in your bucket by prefix - There are many options for
+             * listing the objects in your bucket.  Keep in mind that buckets with
+             * many objects might truncate their results when listing their objects,
+             * so be sure to check if the returned object listing is truncated, and
+             * use the AmazonS3.listNextBatchOfObjects(...) operation to retrieve
+             * additional results.
+             */
+            System.out.println("Listing objects");
+            objectListing = s3.listObjects(new ListObjectsRequest()
+                    .withBucketName(bucketName)
+                    .withPrefix("My"));
+            for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+                System.out.println(" - " + objectSummary.getKey() + "  " +
+                        "(size = " + objectSummary.getSize() + ")");
+            }
+            System.out.println();
 
             /*
              * Delete an object - Unless versioning has been turned on for your bucket,
              * there is no way to undelete an object, so use caution when deleting objects.
              */
-            System.out.println("Deleting an object\n");
+            System.out.println("Deleting objects...\n");
             s3.deleteObject(bucketName, key);
+            s3.deleteObject(bucketName, keylarge);
+            s3.deleteObject(bucketName, keylargeTM);
 
             /*
              * Delete a bucket - A bucket must be completely empty before it can be
@@ -154,6 +356,7 @@ public class S3Sample {
              */
             System.out.println("Deleting bucket " + bucketName + "\n");
             s3.deleteBucket(bucketName);
+            System.out.println("Sucessfully done!");
         } catch (AmazonServiceException ase) {
             System.out.println("Caught an AmazonServiceException, which means your request made it "
                     + "to Amazon S3, but was rejected with an error response for some reason.");
